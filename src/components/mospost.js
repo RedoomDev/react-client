43752d32869df981a3f351e23b27ea45522b5093
Raksix <ermagfurkan7@gmail.com>
2022-01-24 00:02:43 +0300

    hahahaha

--- src/components/mospost.js
+++ src/components/mospost.js
@@ -5,6 +5,7 @@ export function MostPost({ post }) {
       <Link to={"/post/" + post.id}>
          <div className="board-item bg-zinc-800">
             <div className="board-item-head">{post.baslik.length > 20 ? (post.baslik.slice(0, 20) + "...") : (post.baslik)}</div>
+            <hr></hr>
             <div className="board-item-about">{post.icerik.length > 50 ? (post.icerik.slice(0, 50) + "...") : (post.icerik)}</div>
          </div>
       </Link>
