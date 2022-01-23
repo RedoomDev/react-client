56ca2a308faba9e5bccdd6c0d50376cd73521b35
Raksix <ermagfurkan7@gmail.com>
2022-01-23 17:04:52 +0300

    s

--- src/components/boards.js
+++ src/components/boards.js
@@ -1,12 +1,19 @@
+import { BoardContext } from "../contexts/board.context";
 
 
 export function Boards(params) {
 
    return (
       <div className="boards bg-zinc-900">
-         <div className="boards-head">
-            <span className="text-head">Konular</span>
-         </div>
+         <BoardContext.Consumer>
+            {value => (
+               <div className="boards-head">
+                  <span className="text-head">Konular</span>
+                  <div className="cizgi-2"></div>
+                  <div ></div>
+               </div>
+            )}
+         </BoardContext.Consumer>
       </div>
    )
 }
