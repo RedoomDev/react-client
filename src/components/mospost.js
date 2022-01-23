d67b5026a3ac2ea23a98743fde9beb3207fe129c
Raksix <ermagfurkan7@gmail.com>
2022-01-23 17:41:04 +0300

    MostPost component updated

--- src/components/mospost.js
+++ src/components/mospost.js
@@ -2,8 +2,8 @@
 export function MostPost({ post }) {
    return (
       <div className="board-item bg-zinc-800">
-         <div className="board-item-head">{post.baslik}</div>
-         <div className="board-item-about">{post.icerik}</div>
+         <div className="board-item-head">{post.baslik.length > 20 ? (post.baslik.slice(0, 20) + "...") : (post.baslik)}</div>
+         <div className="board-item-about">{post.icerik.length > 50 ? (post.icerik.slice(0, 50) + "...") : (post.icerik)}</div>
       </div>
    )
 }
