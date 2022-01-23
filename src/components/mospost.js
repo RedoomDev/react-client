ca5f243f7fa3ce31dcc577ccfaaaf5365575109a
Raksix <ermagfurkan7@gmail.com>
2022-01-23 17:36:26 +0300

    most posts update

---
+++ src/components/mospost.js
@@ -0,0 +1,9 @@
+
+export function MostPost({ post }) {
+   return (
+      <div className="board-item bg-zinc-800">
+         <div className="board-item-head">{post.baslik}</div>
+         <div className="board-item-about">{post.icerik}</div>
+      </div>
+   )
+}
--- src/components/mostposts.js
+++ src/components/mostposts.js
@@ -1,13 +1,24 @@
+import { PostContext } from "../contexts/post.context";
+import { MostPost } from "./mospost";
 
 
 export function MostPosts(params) {
 
    return (
       <div className="boards-side bg-zinc-900">
-         <div className="boards-head">
-            <span className="text-head">En Yeni Gönderiler</span>
-            <div className="cizgi-2"></div>
-         </div>
+         <PostContext.Consumer>
+            {value => (
+               <div className="boards-head">
+                  <span className="text-head">En Yeni Gönderiler</span>
+                  <div className="cizgi-2"></div>
+                  <div className="boards-items">
+                     {value[0].map(post => (
+                        <MostPost post={post}></MostPost>
+                     ))}
+                  </div>
+               </div>
+            )}
+         </PostContext.Consumer>
       </div>
    )
 }
