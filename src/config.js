26b40f58bc1cb82a7d26f6699e518a54bd440fd6
Raksix <ermagfurkan7@gmail.com>
2022-01-24 23:17:00 +0300

    s

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const api_url = window.location.protocol + window.location.pathname + "/api"
-//export const api_url = "http://localhost:1337/api"
+//export const api_url = window.location.protocol + window.location.pathname + "/api"
+export const api_url = "http://localhost:1337/api"
