18c065c323643a61472ace3bf72740f2bd15b3ee
Raksix <ermagfurkan7@gmail.com>
2022-01-24 23:35:25 +0300

    s

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const api_url = "https://redoom.fun/api"
-//export const api_url = "http://localhost:1337/api"
+//export const api_url = "https://redoom.fun/api"
+export const api_url = "http://localhost:1337/api"
