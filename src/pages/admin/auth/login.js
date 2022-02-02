0931e686f5303424f48f547238b620c9b29a273c
Raksix <ermagfurkan7@gmail.com>
2022-02-02 19:37:10 +0300

    update

--- src/pages/admin/auth/login.js
+++ src/pages/admin/auth/login.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Navbar } from "../../../components/navbar"
 import { LoginPost } from "../../../fetch/fetchs"
 
 
@@ -12,38 +13,42 @@ export default function AdminLogin() {
    const [click, setClick] = useState(false)
 
    return (
-      <div className="window bg-zinc-900" style={{ color: 'white' }}>
-         <div className="form">
-            <div className="post-error">{err}</div>
-            <div className="form-label">E-Posta</div>
-            <input className="form-input bg-zinc-800" onChange={(e) => {
-               setEmail(e.target.value)
-            }}></input>
-            <div className="form-label">Şifre</div>
-            <input className="form-input bg-zinc-800" type="password" onChange={(e) => {
-               setPassword(e.target.value)
-            }}></input>
 
-            <div style={{ height: 30 }}></div>
-            {click === false ? (
-               <div className="form-button bg-zinc-800" type="button" onClick={() => {
-                  setClick(true)
-                  LoginPost({
-                     email,
-                     password
-                  }).then(res => {
-                     if(res.token){
-                        localStorage.setItem("token", res.token)
-                        window.location.href = ""
-                     }else{
-                        setErr(res.message)
-                        setClick(false)
-                     }
-                  })
-               }}>Giriş Yap</div>
-            ) : (
-               <div className="form-button bg-zinc-800" type="button" aria-disabled="true">Sunucuya İstek Gönderiliyor...</div>
-            )}
+      <div style={{ color: 'white' }}>
+         <Navbar></Navbar>
+         <div className="window bg-zinc-900" style={{ color: 'white' }}>
+            <div className="form">
+               <div className="form-head">Giriş Yap</div>
+               <div className="post-error">{err}</div>
+               <div className="form-label">E-Posta</div>
+               <input className="form-input bg-zinc-800" onChange={(e) => {
+                  setEmail(e.target.value)
+               }}></input>
+               <div className="form-label">Şifre</div>
+               <input className="form-input bg-zinc-800" type="password" onChange={(e) => {
+                  setPassword(e.target.value)
+               }}></input>
+
+               {click === false ? (
+                  <div className="form-button bg-zinc-800" type="button" onClick={() => {
+                     setClick(true)
+                     LoginPost({
+                        email,
+                        password
+                     }).then(res => {
+                        if (res.token) {
+                           localStorage.setItem("token", res.token)
+                           window.location.href = ""
+                        } else {
+                           setErr(res.message)
+                           setClick(false)
+                        }
+                     })
+                  }}>Giriş Yap</div>
+               ) : (
+                  <div className="form-button bg-zinc-800" type="button" aria-disabled="true">Sunucuya İstek Gönderiliyor...</div>
+               )}
+            </div>
          </div>
       </div>
    )
