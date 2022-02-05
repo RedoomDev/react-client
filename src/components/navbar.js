3f5399996f58922ef6c91f618c9d6bbd514bdba5
Raksix <ermagfurkan7@gmail.com>
2022-02-06 00:55:58 +0300

    serach button

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,16 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { BoardContext } from "../contexts/board.context";
 
 
 export function Navbar() {
 
+   const [input, setInput] = useState("")
+
    return (
       <BoardContext.Consumer>
          {value => (
             <div className="w-screen bg-zinc-900 ">
                <div style={{ paddingTop: 10 }}></div>
-               <Link to="/" className="text-5xl" style={{ margin: '2.5vw' }}>Redoom</Link>
+               <div id="navbar-main">
+                  <Link to="/" className="text-5xl" style={{ margin: '2.5vw' }}>Redoom</Link>
+                  <div id="main">
+                     <input id="search-input" type="search" name="q" placeholder="Bir şeyler ara..." value={input} autocomplete="off"
+                        pattern="[^'\x22]+" onChange={(e) => {
+                           setInput(e.target.value)
+                        }} />
+                     <Link to={"/search?query=" + input}>
+                        <button id="search-button" type="button">Ara</button>
+                     </Link>
+                  </div>
+               </div>
                <div className="cizgi"></div>
                <div className="text-2xl navbar-boards">
                   {value[0].map((a, idx) => (
